dbdac8e4bf0ba3a450715cf6a6e92a3c3720add4
Greg Perrin <gregoryperrin.ucb@gmail.com>
2023-09-13 13:23:04 -0600

    Header.

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./store"
 import App from "./App"
+import Header from "./Header"
 import "./index.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Header>
+        <App />
+      </Header>
     </Provider>
   </React.StrictMode>,
 )
