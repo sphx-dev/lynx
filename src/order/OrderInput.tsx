bb6011d6c8981088d25d4bffcdc8770b440aee7f
Greg Perrin <gregoryperrin.ucb@gmail.com>
2023-10-13 07:03:00 -0600

    Add long/short to order input.

--- src/order/OrderInput.tsx
+++ src/order/OrderInput.tsx
@@ -1,30 +1,39 @@
 import Button from "react-bootstrap/Button"
+import ButtonGroup from "react-bootstrap/ButtonGroup"
 import Card from "react-bootstrap/Card"
 import Form from "react-bootstrap/Form"
 import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 function OrderInput() {
   return (
     <>
       <Card style={{ backgroundColor: "#e5e5e5" }}>
         <Card.Body>
+          <Row>
+            <Col xs={10}>
+              <div style={{ marginTop: "5px", marginLeft: "10px" }}>
+                Oil / USDC
+              </div>
+            </Col>
+            <Col>
+              <ButtonGroup aria-label="long-short">
+                <Button variant="primary">Long</Button>
+                <Button variant="secondary">Short</Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+
           <Card style={{ marginTop: "1rem", marginBottom: "1rem" }}>
             <Card.Body>
               <Form>
-                <Row>
-                  <Form.Check // prettier-ignore
-                    type="switch"
-                    id="short-switch"
-                    label="Short"
-                  />
-                </Row>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" controlId="formPrice">
                   <Form.Label>Price</Form.Label>
-                  <Form.Control type="float" placeholder="85.00" />
+                  <Form.Control type="float" placeholder="0.00" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="formVolume">
                   <Form.Label>Amount</Form.Label>
-                  <Form.Control type="number" placeholder="Long amount" />
+                  <Form.Control type="number" placeholder="Amount" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                   Place my trade
