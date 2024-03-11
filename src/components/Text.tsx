5b34b4b43aa7831e3d24b1d47e1d361cf892547e
vossaba <vobatsan@gmail.com>
2024-03-11 20:52:00 +0200

    add Text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import styled from "styled-components"
+
+import fonts from "../theme/fonts"
+
+type Align = "left" | "center" | "right"
+type Tag = "span" | "p"
+
+interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
+  variant?: keyof typeof fonts.typography
+  align?: Align
+  color?: string
+  as?: Tag
+}
+
+const StyledText = styled.p<TextProps>`
+  ${({ variant, theme }) => {
+    return theme.fonts.typography[variant || "default"]
+  }};
+  text-align: ${({ align }) => align};
+  color: ${({ color, theme }) =>
+    color || theme.color.selectedTheme.text.default};
+`
+
+const Text = ({
+  variant,
+  color,
+  align = "left",
+  children,
+  as = "p",
+}: TextProps) => {
+  return (
+    <StyledText as={as} align={align} variant={variant} color={color}>
+      {children}
+    </StyledText>
+  )
+}
+
+export default Text
