941de15bd5f5ff45f1a456a7955ab80123b81763
Vlad <vlad.vorob.pro@gmail.com>
2024-04-17 09:22:46 +0300

    fix layout

--- src/sections/chart/style.tsx
+++ src/sections/chart/style.tsx
@@ -4,5 +4,5 @@ export const TVChartContainer = styled.div`
   //height: calc(100vh - 72px - 95px - 28px - 140px);
   //align-self: stretch;
   flex: 1 1 auto;
-  margin-top: 8px;
+  margin-top: 12px;
 `
