fbeda0fb3a791f4524af3e0a495bc87372dd645f
Vlad <vlad.vorob.pro@gmail.com>
2024-04-17 16:15:08 +0300

    fixes after review

--- src/Layouts/BaseTemplate.tsx
+++ src/Layouts/BaseTemplate.tsx
@@ -1,16 +1,8 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
-import styled from "styled-components"
 
-const Wrapper = styled.div`
-  //height: calc(100vh - 28px - 95px);
-`
 const BaseTemplate = () => {
-  return (
-    <Wrapper>
-      <Outlet />
-    </Wrapper>
-  )
+  return <Outlet />
 }
 
 export default BaseTemplate
--- src/sections/chart/style.tsx
+++ src/sections/chart/style.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components"
 
 export const TVChartContainer = styled.div`
-  //height: calc(100vh - 72px - 95px - 28px - 140px);
-  //align-self: stretch;
   flex: 1 1 auto;
   margin-top: 12px;
 `
