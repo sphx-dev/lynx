f52fd5ec79408f831bbab5d34318f0093fb0eec3
Vlad <162363476+vladvorob@users.noreply.github.com>
2024-06-10 19:05:08 +0300

    clean a logo area in the app header. (#61)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,66 +1,28 @@
 import styled from "styled-components";
-import logout from "../../assets/icons/logout.svg";
-import myAccount from "../../assets/icons/account.svg";
 import logo from "../../assets/icons/logo-dark.svg";
 import Group from "../Group";
 import Nav from "../../sections/navbar";
-import headerBg from "../../assets/images/header-bg.png";
+import headerBg from "../../assets/images/header-logo-bg.svg";
 
 const StyledHeader = styled.header`
   position: relative;
   background-image: url(${headerBg});
-  background-position: top;
+  background-repeat: no-repeat;
+  //background-position: top;
   width: 320px;
   height: 100px;
-  margin-top: -5px;
-`;
-
-const NavBar = styled.nav`
-  position: absolute;
-  top: 70px;
-  display: flex;
-  justify-content: space-evenly;
-  width: 80%;
-`;
-const Button = styled.div`
-  color: ${({ theme }) => theme.colors.common.palette.secondary["700"]};
-  ${({ theme }) => theme.fonts.typography.header};
-  cursor: pointer;
-  display: flex;
-  img {
-    margin-right: 4px;
-    margin-bottom: 2px;
-    height: 14px;
-  }
-  &:hover {
-    color: ${({ theme }) => theme.colors.common.palette.secondary["100"]};
-  }
 `;
 
 const Logo = styled.img`
-  padding-top: 12px;
-  margin-left: 64px;
+  padding-top: 15px;
+  margin-left: 45px;
 `;
 
-const Divider = styled.div`
-  border-left: 1px solid #173d46;
-`;
 const Header = () => {
   return (
     <Group fluid spacing={0} style={{ width: "100vw" }}>
       <StyledHeader>
         <Logo src={logo} />
-        <NavBar>
-          <Button>
-            <img src={myAccount} alt="my account" />
-            MY ACCOUNT
-          </Button>
-          <Divider />
-          <Button>
-            <img src={logout} alt="my account" />
-            LOGOUT
-          </Button>
-        </NavBar>
         {/*<span>Logout</span>*/}
       </StyledHeader>
       <Nav />
