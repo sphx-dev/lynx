7451f2246f37807ea3779747b82066bd536f1882
Scipion <zapador50@gmail.com>
2024-10-01 18:03:35 +0200

    refactor: change denom by default from uusdc to usdc

--- src/constants.ts
+++ src/constants.ts
@@ -6,9 +6,6 @@ export const MOBILE_WIDTH: number = 800;
 // Order book rows
 export const ORDERBOOK_LEVELS: number = 18;
 
-// BASE API URL
-export const API_URL: string = config?.VITE_API_URL;
-
 export const BREAK_POINTS = {
   DESKTOP_MIN_WIDTH: 1200,
   TABLET_MIN_WIDTH: 800,
