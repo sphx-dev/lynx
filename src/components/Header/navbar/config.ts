bbb99478edd3531a22a91fbbfa350166e8b983fc
Scipion <zapador50@gmail.com>
2025-05-06 16:58:03 +0200

    Update header links

--- src/components/Header/navbar/config.ts
+++ src/components/Header/navbar/config.ts
@@ -2,41 +2,31 @@ import config from "@/config";
 import { ROUTES } from "../../../routes";
 
 export const navLinks = [
-  {
-    title: "Dashboard",
-    path: ROUTES.dashboard,
-    icon: null,
-  },
+  // {
+  //   title: "Dashboard",
+  //   path: ROUTES.dashboard,
+  //   icon: null,
+  // },
   {
     title: "Exchange",
     path: ROUTES.exchange,
     icon: null,
   },
-  {
-    title: "Faucet",
-    path: ROUTES.dashboard + "/" + ROUTES.dashboardFaucet,
-    icon: null,
-  },
-  {
-    title: "Station",
-    path: config.VITE_STATION_URL,
-    icon: null,
-    target: "_blank",
-  },
+  // {
+  //   title: "Faucet",
+  //   path: ROUTES.dashboard + "/" + ROUTES.dashboardFaucet,
+  //   icon: null,
+  // },
+  // {
+  //   title: "Station",
+  //   path: config.VITE_STATION_URL,
+  //   icon: null,
+  //   target: "_blank",
+  // },
   {
     title: "Explorer",
     path: config.VITE_EXPLORER_URL,
     icon: null,
     target: "_blank",
   },
-  // {
-  //   title: "Leaderboard",
-  //   path: ROUTES.leaderBoard,
-  //   icon: null,
-  // },
-  // {
-  //   title: "referrals",
-  //   path: ROUTES.referrals,
-  //   icon: null,
-  // },
 ];
